ff1c93f6491142d7171023488444fb1e9c0e802f
Angelo Chaves <angeloechaves@gmail.com>
2014-10-06 01:25:00 -0300

    Changed board varchar size to allow serialization

--- config/database.sql
+++ config/database.sql
@@ -1,9 +1,13 @@
+DROP DATABASE IF EXISTS users_16384;
+CREATE DATABASE users_16384;
+
+USE users_16384;
+
 DROP TABLE IF EXISTS users; -- Users
 CREATE TABLE users(
-    id INT(11) NOT NULL AUTO_INCREMENT,
     nickname VARCHAR(255),
     password VARCHAR(255),
-    board VARCHAR(255),
+    board VARCHAR(2555),
     has_unfinished_game BOOLEAN,
-  CONSTRAINT users_pk PRIMARY KEY (id)
+  CONSTRAINT users_pk PRIMARY KEY (nickname)
 );
